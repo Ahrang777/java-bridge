c1f748f50000f13c5ba68046308cc219a9547741
JunginPark4364 <wjddlsee@gmail.com>
2022-12-13 16:17:45 +0900

    feat(OutputView): 에러 메시지 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -8,6 +8,7 @@ public class OutputView {
     private static final String BRIDGE_SIZE_INPUT_MESSAGE = "\n다리의 길이를 입력해주세요.";
     private static final String MOVING_INPUT_MESSAGE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String GAME_COMMAND_INPUT_MESSAGE = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String ERROR = "[ERROR] ";
 
     public void printStartMessage() {
         System.out.println(START_MESSAGE);
@@ -23,6 +24,10 @@ public class OutputView {
 
     public void printGameCommandInputMessage() {
         System.out.println(GAME_COMMAND_INPUT_MESSAGE);
+    }
+
+    public void printErrorMessage(Exception exception) {
+        System.out.println(ERROR + exception.getMessage());
     }
 
     /**
