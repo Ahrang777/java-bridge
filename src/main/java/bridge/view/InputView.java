f55b16bc82a250297a67cd0795c3f7e3e2e4379f
JunginPark4364 <wjddlsee@gmail.com>
2022-12-13 17:18:02 +0900

    refactor(InputView): 매개변수에 final 키워드 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -14,7 +14,7 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public BridgeSizeDto readBridgeSize(OutputView outputView) {
+    public BridgeSizeDto readBridgeSize(final OutputView outputView) {
         outputView.printBridgeSizeInputMessage();
         try {
             String inputBridgeSize = input();
